313970657fbefeaefc780443e829654fa9d6c6fd
Özgürhan Polat <ozgurhan.45@gmail.com>
2024-03-01 01:54:54 +0300

    Sidebasr is completed almost

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,5 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Sidebar.css";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import profilePhoto from "../../images/Profile.svg";
 import anonimPhoto from "../../images/icons/anonim.svg";
 import hocalarPhoto from "../../images/icons/hocalar.svg";
@@ -9,13 +11,111 @@ import bolumlerPhoto from "../../images/icons/bölümler.svg";
 import mekanlarPhoto from "../../images/icons/mekanlar.svg";
 import messagePhoto from "../../images/icons/mesajlarım.svg";
 import logoutPhoto from "../../images/icons/cikisyap.svg";
+import iyteCarPhoto from "../../images/icons/iyte_car_icon.svg";
 
-const Sidebar = ({ email, name, surname,isOpen }) => {
-  
+const Sidebar = ({ isOpen }) => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const navigation = useNavigate();
+  const [activeElement, setActiveElement] = useState();
+
+  const user = useSelector((state) => state.user.user);
+
+  useEffect(() => {
+    setName(user.name);
+    setSurname(user.surname);
+    setEmail(setEmail);
+    const anonymousElement = document.querySelector("#anonymous");
+    setActiveElement(anonymousElement);
+    anonymousElement.classList.add("active");
+  }, [email, name, surname]);
+
+  // Handle Navigations
+
+  const handleAnonymous = () => {
+    const anonymousElement = document.querySelector("#anonymous");
+    if (!anonymousElement.classList.contains("active")) {
+      anonymousElement.classList.add("active");
+      activeElement.classList.remove("active");
+      setActiveElement(anonymousElement);
+    }
+    navigation("/anonymous");
+  };
+  const handleTeacher = () => {
+    const teacherElement = document.querySelector("#teacher");
+    if (!teacherElement.classList.contains("active")) {
+      teacherElement.classList.add("active");
+      activeElement.classList.remove("active");
+      setActiveElement(teacherElement);
+    }
+    navigation("/teachers");
+  };
+  const handleLesson = () => {
+    const lessonElement = document.querySelector("#lesson");
+    if (!lessonElement.classList.contains("active")) {
+      lessonElement.classList.add("active");
+      activeElement.classList.remove("active");
+      setActiveElement(lessonElement);
+    }
+    navigation("/lessons");
+  };
+  const handleCommunity = () => {
+    const communityElement = document.querySelector("#community");
+    if (!communityElement.classList.contains("active")) {
+      communityElement.classList.add("active");
+      activeElement.classList.remove("active");
+      setActiveElement(communityElement);
+    }
+    navigation("/communities");
+  };
+
+  const handleDepartment = () => {
+    const departmentElement = document.querySelector("#department");
+    if (!departmentElement.classList.contains("active")) {
+      departmentElement.classList.add("active");
+      activeElement.classList.remove("active");
+      setActiveElement(departmentElement);
+    }
+    navigation("/departments");
+  };
+  const handlePlace = () => {
+    const placeElement = document.querySelector("#place");
+    if (!placeElement.classList.contains("active")) {
+      placeElement.classList.add("active");
+      activeElement.classList.remove("active");
+      setActiveElement(placeElement);
+    }
+    navigation("/places");
+  };
+  const handleMessage = () => {
+    const myMessageElement = document.querySelector("#message");
+    if (!myMessageElement.classList.contains("active")) {
+      myMessageElement.classList.add("active");
+      activeElement.classList.remove("active");
+      setActiveElement(myMessageElement);
+    }
+    navigation("my-messages");
+  };
+  const handleIyteCar = () => {
+    const iyteCarElement = document.querySelector("#iytecar");
+    if (!iyteCarElement.classList.contains("active")) {
+      iyteCarElement.classList.add("active");
+      activeElement.classList.remove("active");
+      setActiveElement(iyteCarElement);
+    }
+    navigation("iyte-car");
+  };
+  const handleLogout = () => {
+    navigation("/login");
+  };
+
   return (
     <div
       id="bdSidebar"
-      className={`d-flex flex-column flex-shrink-0 p-3 offcanvas-lg offcanvas-start ${isOpen ? 'show' : ''}`}
+      className={`d-flex flex-column flex-shrink-0 p-3 offcanvas-lg offcanvas-start ${
+        isOpen ? "show" : ""
+      }`}
       style={{ width: "280px" }}
     >
       <div
@@ -38,51 +138,58 @@ const Sidebar = ({ email, name, surname,isOpen }) => {
 
       <ul className="mynav nav nav-pills flex-column mb-auto">
         <li className="nav-item mb-3">
-          <a href="../anonimler/index.html" className="">
+          <a id="anonymous" href="" className="" onClick={handleAnonymous}>
             <img src={anonimPhoto} className="p-1" alt="" />
-            Anonimler
+            <span className="ms-2">Anonimler</span>
           </a>
         </li>
         <li className="nav-item mb-3">
-          <a href="#" className="">
+          <a id="iytecar" href="#" className="" onClick={handleIyteCar}>
+            <img src={iyteCarPhoto} className="p-1" alt="" />
+            <span className="ms-2">İyte Araç</span>
+          </a>
+        </li>
+        <li className="nav-item mb-3">
+          <a id="teacher" href="#" className="" onClick={handleTeacher}>
             <img src={hocalarPhoto} className="p-1" alt="" />
-            Hocalar
+            <span className="ms-2">Hocalar</span>
           </a>
         </li>
         <li className="nav-item mb-3">
-          <a href="#" className="">
+          <a id="lesson" href="#" className="" onClick={handleLesson}>
             <img src={lessonsPhoto} className="p-1" alt="" />
-            Dersler
+            <span className="ms-2">Dersler</span>
           </a>
         </li>
         <li className="nav-item mb-3">
-          <a href="#" className="active">
+          <a id="community" href="#" className="" onClick={handleCommunity}>
             <img src={topluluklarPhoto} className="p-1" alt="" />
-            Topluluklar
+            <span className="ms-2">Topluluklar</span>
           </a>
         </li>
         <li className="nav-item mb-3">
-          <a href="#" className="">
+          <a id="department" href="#" className="" onClick={handleDepartment}>
             <img src={bolumlerPhoto} className="p-1" alt="" />
-            Bölümler
+            <span className="ms-2">Bölümler</span>
           </a>
         </li>
         <li className="nav-item mb-3">
-          <a href="#" className="">
+          <a id="place" href="#" className="" onClick={handlePlace}>
             <img src={mekanlarPhoto} className="p-1" alt="" />
-            Mekanlar
+            <span className="ms-2">Mekanlar</span>
           </a>
         </li>
         <li className="nav-item mb-3">
-          <a href="#" className="">
+          <a id="message" href="#" className="" onClick={handleMessage}>
             <img src={messagePhoto} className="p-1" alt="" />
-            Mesajlarım
+            <span className="ms-2">Mesajlarım</span>
           </a>
         </li>
+
         <li className="nav-item mb-3">
-          <a href="#" className="">
+          <a id="logout" href="#" className="" onClick={handleLogout}>
             <img src={logoutPhoto} className="p-1" alt="" />
-            Çıkış Yap
+            <span className="ms-2">Çıkış Yap</span>
           </a>
         </li>
       </ul>
